d2a5ded97a6dc9171adf00e78dfdbcf1438ce541
seogyeong1 <d110415@naver.com>
2023-05-17 22:37:00 +0900

    0517

---
+++ app/src/main/java/hnu/multimedia/todo/FindIdActivity.java
@@ -0,0 +1,14 @@
+package hnu.multimedia.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindIdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_id);
+    }
+}
