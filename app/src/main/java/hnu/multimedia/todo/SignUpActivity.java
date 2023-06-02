3391fee982e8318592bd32524cdcb8b630c9a9b8
seogyeong1 <82697951+seogyeong1@users.noreply.github.com>
2023-06-02 20:44:39 +0900

    0601

--- app/src/main/java/hnu/multimedia/todo/SignUpActivity.java
+++ app/src/main/java/hnu/multimedia/todo/SignUpActivity.java
@@ -1,5 +1,7 @@
 package hnu.multimedia.todo;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -17,14 +19,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    Button btnIDCheck;
+    EditText etID;
+    EditText etPassword;
+    EditText etPasswordTest;
+    EditText etNickname;
+
+    boolean isDidIdCheck = false;
+    boolean isDidPasswordCheck = false;
+    boolean isDidEmailCheck = false;
+
+
+
+    @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        Button btnIDCheck = findViewById(R.id.btnIDCheck);
-        EditText etID = findViewById(R.id.etID);
-        EditText etPassword = findViewById(R.id.etPassword);
-        EditText etPasswordTest = findViewById(R.id.etPasswordTest);
+        btnIDCheck = findViewById(R.id.btnIDCheck);
+        etID = findViewById(R.id.etID);
+        etPassword = findViewById(R.id.etPassword);
+        etPasswordTest = findViewById(R.id.etPasswordTest);
+        etNickname = findViewById(R.id.etNickname);
+
         String email;
 
 
@@ -33,10 +50,12 @@ public class SignUpActivity extends AppCompatActivity {
         btnIDCheck.setOnClickListener(view -> {
             if (etID.getText().length() < 1) {
                 Toast.makeText(SignUpActivity.this, "아이디를 입력하세요", Toast.LENGTH_LONG).show();
-                return;
             }
             //TODO: 아이디 중복 검사
-            return;
+            // 아이디를 중복 검사했는데 중복이 아닌 걸로 나왔다!
+            if(etID.getText().toString().equals("aaa")) {
+                isDidIdCheck = true;
+            }
         });
 
         // 패스워드 일치 검사
@@ -50,6 +69,7 @@ public class SignUpActivity extends AppCompatActivity {
                 if (etPassword.getText().toString().equals(etPasswordTest.getText().toString())) {
                     TextView tvPasswordSame = findViewById(R.id.tvPasswordSame);
                     tvPasswordSame.setText("비밀번호가 일치합니다.");
+                    isDidPasswordCheck = true;
                 }
             }
 
@@ -74,7 +94,7 @@ public class SignUpActivity extends AppCompatActivity {
                 Toast.makeText(SignUpActivity.this, "이메일을 입력하세요2", Toast.LENGTH_LONG).show();
                 return;
             }
-            Log.d("email", etEmail.getText().toString()+"@"+spnEmail.getSelectedItem().toString());
+            isDidEmailCheck = true;
         });
 
         // TODO: emil + @ + Spinner 을 통해 이메일 중복 확인
@@ -94,12 +114,42 @@ public class SignUpActivity extends AppCompatActivity {
         // 각 중복검사들 함수로 만들어서 boolean type 리턴...?? 인가?
        Button btnJoin = findViewById(R.id.btnJoin);
        btnJoin.setOnClickListener(v -> {
-           // 이메일 중복 체크
-           // 아이디 중복 체크
-           // 비밀번호 일치 체크
-           if (etPassword.getText().toString().equals(etPasswordTest.getText().toString())){
-
+           // 아이디 입력을 안 했을 시
+           if(etID.getText().toString().length() < 1) {
+               Toast.makeText(SignUpActivity.this, "아이디를 입력하세요", Toast.LENGTH_LONG).show();
            }
+
+           // 비밀번호 입력을 안 했을 시
+           if(etPassword.getText().toString().length() < 1
+                   || etPasswordTest.getText().toString().length() < 1) {
+               Toast.makeText(SignUpActivity.this, "비밀번호를를 입력하세요", Toast.LENGTH_LONG).show();
+           }
+
+           // 닉네임 입력 안 했을 시
+           if(etNickname.getText().toString().length() < 1) {
+               Toast.makeText(SignUpActivity.this, "닉네임을 입력하세요", Toast.LENGTH_LONG).show();
+           }
+
+           // 이메일 입력 안 했을 시
+           if (etEmail.getText().length() < 1) {
+               Toast.makeText(SignUpActivity.this, "이메일을 입력하세요", Toast.LENGTH_LONG).show();
+               return;
+           }
+           if(!spnEmail.getSelectedItem().toString().contains("com") &&
+                   !spnEmail.getSelectedItem().toString().contains("net")) {
+               Toast.makeText(SignUpActivity.this, "이메일을 입력하세요2", Toast.LENGTH_LONG).show();
+               return;
+           }
+
+           // 중복검사
+          if(isDidEmailCheck && isDidPasswordCheck && isDidIdCheck
+           && etNickname.getText().length() > 0) {
+               Intent intent= new Intent(SignUpActivity.this, MainActivity.class);
+               startActivity(intent);
+           } else if (!isDidIdCheck) {
+              Toast.makeText(SignUpActivity.this, "아이디 중복 검사", Toast.LENGTH_LONG).show();
+          }
+
        });
     }
 }
